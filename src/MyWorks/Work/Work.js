e01f7d500276436c2ada3b8d8cce627e1ef78134
ches- <Es18.03.88@gmail.com>
2021-03-24 03:36:26 +0300

    css

--- src/MyWorks/Work/Work.js
+++ src/MyWorks/Work/Work.js
@@ -7,8 +7,7 @@ let Work = (props) => {
     return (
         <div className={s.work}>
             <div className={s.icon}>
-                <img src="https://vuzopedia.ru/storage/app/uploads/public/5b3/8b0/bf6/5b38b0bf60a2f780799494.jpg"
-                     alt="image work"/>
+                <a href="" className={s.link}>LOOK</a>
             </div>
             <h3 className={s.title}>{props.title}</h3>
             <span className={s.description}>{props.description}</span>
