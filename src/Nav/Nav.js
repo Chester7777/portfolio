fdb5baee70f5b5edc11f1e6def5e06efd7bd818e
Es180 <Es18.03.88>
2021-05-07 00:53:17 +0300

    SCSS, mixins, variables, animation

--- src/Nav/Nav.js
+++ src/Nav/Nav.js
@@ -2,12 +2,13 @@ import React from "react"
 import s from "./Nav.module.scss"
 
 
-
 let Nav = () => {
 
     return (
         <div className={s.menu}>
-            <a href=''>Home page</a>
+            <a href=''>
+                <span className={s.primaryColor}>Home page</span>
+            </a>
             <a href=''>Skills</a>
             <a href=''>Projects</a>
             <a href=''>Contacts</a>
