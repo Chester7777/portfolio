5695f60c85ac7fc0fd02c3027ed5befe0b8b91da
Es180 <Es18.03.88@gmail.com>
2021-06-27 12:34:19 +0300

    adaptation

--- src/MyWorks/MyWorks.js
+++ src/MyWorks/MyWorks.js
@@ -29,7 +29,8 @@ let MyWorks = () => {
                           href="https://chester7777.github.io/todolist_16/"/>
                     <Work style={socialNetwork} title={"SOCIAL NETWORK"} description={SOCIAL_NETWORK}
                           href="https://chester7777.github.io/sn-ts-example/"/>
-                    <Work style={socialNetwork} title={"CONVERTER"} description={CONVERTER} href=""/>
+                    <Work style={socialNetwork} title={"CONVERTER"} description={CONVERTER}
+                          href="https://chester7777.github.io/Converter/"/>
                     <Work style={socialNetwork} title={"TODOLIST 2"} description={TODOLIST2}
                           href="https://chester7777.github.io/NewToDoForDay_JS/"/>
                     <Work style={socialNetwork} title={"PORTFOLIO"} description={PORTFOLIO}
